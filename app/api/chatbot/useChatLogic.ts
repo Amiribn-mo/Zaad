9b6f14efb38df253e20ca0834c82d08d3bacaa95
Amiribn-mo-1 <amiribnumo@gmail.com>
2025-06-13 15:14:41 +0300

    hh

--- app/api/chatbot/useChatLogic.ts
+++ app/api/chatbot/useChatLogic.ts
@@ -128,7 +128,6 @@ export const useChatLogic = () => {
     isSignedIn,
     user,
     isLoaded,
-    close,
     handleInputChange,
     handleClick,
   };
--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -18,7 +18,7 @@ export default function PromptInput() {
     lastMessageRef,
     isSignedIn,
     user,
-    close,
+  
    isLoaded,
     // Removed
     handleInputChange,
